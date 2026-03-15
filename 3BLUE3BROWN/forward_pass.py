190c68800cab378549cb396c7ad7d30147a54e13
blacklily17 <xj13552740132@163.com>
2026-03-15 20:57:32 +0800

    Update forward_pass.py

--- 3BLUE3BROWN/forward_pass.py
+++ 3BLUE3BROWN/forward_pass.py
@@ -38,6 +38,39 @@ def compute_cost(a3,y_true):
     cost = np.sum(np.square(a3 - y_true))
     return cost
 
+def sigmoid_derivative(a):
+    return sigmoid(a) * (1 - sigmoid(a))
+
+def backward_propagation(a0, a1, a2, a3,y_true):
+    dz3 = (a3 - y_true) * sigmoid_derivative(a3)
+    dw3 = np.dot(dz3, a2.T)
+    db3 = dz3
+
+    dz2 = np.dot(w3.T,dz3) * sigmoid_derivative(a2)
+    dw2 = np.dot(dz2, a2.T)
+    db2 = dz2
+
+    dz1 = np.dot(w2.T,dz2) * sigmoid_derivative(a1)
+    dw1 = np.dot(dz1, a0.T)
+    db1 = dz1
+
+    return dw1, dw2, dw3, db1, db2, db3
+
+
+# 10. 梯度下降（更新旋钮）
+def update_parameters(dW1, db1, dW2, db2, dW3, db3, learning_rate=0.1):
+    global W1, b1, W2, b2, W3, b3  # 声明我们要修改外面的全局变量
+
+    # 新权重 = 老权重 - 学习率 * 梯度
+    W3 = W3 - learning_rate * dW3
+    b3 = b3 - learning_rate * db3
+
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+
 #随机生成图片a0
 if __name__ == "__main__":
     picture = np.random.rand(layer0_size,1)
